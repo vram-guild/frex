16ecd29ce5882824b7babf9e2aaf3709aa0667ea
spiralhalo <re.nanashi95@gmail.com>
2023-11-18 22:53:51 +0700

    Fix sprite indexing

--- common/src/main/java/io/vram/frex/mixin/MixinSpriteLoader.java
+++ common/src/main/java/io/vram/frex/mixin/MixinSpriteLoader.java
@@ -20,40 +20,55 @@
 
 package io.vram.frex.mixin;
 
-import java.util.Map;
+import java.util.List;
+import java.util.concurrent.Executor;
 
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import net.minecraft.client.renderer.texture.SpriteContents;
 import net.minecraft.client.renderer.texture.SpriteLoader;
 import net.minecraft.client.renderer.texture.TextureAtlas;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
-import net.minecraft.resources.ResourceLocation;
 
 import io.vram.frex.impl.texture.IndexedSprite;
 import io.vram.frex.impl.texture.SpriteIndexImpl;
+import io.vram.frex.mixinterface.SpriteLoaderExt;
 
-@Mixin(TextureAtlas.class)
-public class MixinTextureAltasCommon {
-	@Shadow @Final private ResourceLocation location;
-	@Shadow @Final private Map<ResourceLocation, TextureAtlasSprite> texturesByName;
+@Mixin(SpriteLoader.class)
+public class MixinSpriteLoader implements SpriteLoaderExt {
+	@Unique
+	private TextureAtlas frx_textureAtlas;
 
-	@Inject(at = @At("RETURN"), method = "upload")
-	private void afterUpload(SpriteLoader.Preparations input, CallbackInfo ci) {
+	@Inject(at = @At("RETURN"), method = "create")
+	private static void onCreate(TextureAtlas textureAtlas, CallbackInfoReturnable<SpriteLoader> cir) {
+		((SpriteLoaderExt) cir.getReturnValue()).frx_acceptAtlas(textureAtlas);
+	}
+
+	@Inject(at = @At("RETURN"), method = "stitch")
+	private void afterStitch(List<SpriteContents> list, int i, Executor executor, CallbackInfoReturnable<SpriteLoader.Preparations> cir) {
+		final var preparations = cir.getReturnValue();
+
 		final ObjectArrayList<TextureAtlasSprite> spriteIndexList = new ObjectArrayList<>();
 		int index = 0;
 
-		for (final TextureAtlasSprite sprite : texturesByName.values()) {
+		for (final TextureAtlasSprite sprite : preparations.regions().values()) {
 			spriteIndexList.add(sprite);
 			final var spriteExt = (IndexedSprite) sprite;
 			spriteExt.frex_index(index++);
 		}
 
-		SpriteIndexImpl.getOrCreate(location).reset(input, spriteIndexList, (TextureAtlas) (Object) this);
+		SpriteIndexImpl.getOrCreate(frx_textureAtlas.location()).reset(preparations, spriteIndexList, frx_textureAtlas);
+
+		this.frx_textureAtlas = null;
+	}
+
+	@Override
+	public void frx_acceptAtlas(TextureAtlas atlas) {
+		frx_textureAtlas = atlas;
 	}
 }
